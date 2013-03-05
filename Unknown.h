a328387145a42a74feb8b0f0ccd50aa054f6e87c
scottrick <scottrick49@gmail.com>
2013-03-05 17:36:24 -0600

    More tab fixes...

--- Unknown.h
+++ Unknown.h
@@ -2,19 +2,19 @@
 
 class Unknown {
 public:
-	Unknown();
-	
-	virtual void                AddRef();
-	virtual void                Release();
+    Unknown();
+
+    virtual void                AddRef();
+    virtual void                Release();
     int                         GetRefCount() const;
-	
+
     virtual const char          *GetClassName() const   = 0;
-	
+
 protected:
-	virtual ~Unknown();
+    virtual ~Unknown();
 
     int                         m_RefCount;
-	
+
 private:
-	void                        Init();
+    void                        Init();
 };
