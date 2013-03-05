bb310f5d966b750860b50583e139f18614944427
scottrick <scottrick49@gmail.com>
2013-03-05 17:34:56 -0600

    Fixed tabs

--- Unknown.h
+++ Unknown.h
@@ -4,17 +4,17 @@ class Unknown {
 public:
 	Unknown();
 	
-	virtual void				AddRef();
-	virtual void				Release();
-    int							GetRefCount() const;
+	virtual void                AddRef();
+	virtual void                Release();
+    int                         GetRefCount() const;
 	
-    virtual const char			*GetClassName() const   = 0;
+    virtual const char          *GetClassName() const   = 0;
 	
 protected:
 	virtual ~Unknown();
 
-    int							m_RefCount;
+    int                         m_RefCount;
 	
 private:
-	void						Init();
+	void                        Init();
 };
